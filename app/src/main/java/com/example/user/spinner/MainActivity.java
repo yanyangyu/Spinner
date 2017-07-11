422b9a87189426b498af5cc44f122cb68b2c60f8
yanyangyu <40441114@gm.nfu.edu.tw>
2017-07-11 10:54:26 +0800

    驗證選項內容被選取 並且顯示在 textview 上

--- app/src/main/java/com/example/user/spinner/MainActivity.java
+++ app/src/main/java/com/example/user/spinner/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.user.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+    TextView textView;
     String [] fruit =new String[]{"香蕉","蘋果","鳳梨"};
 
 
@@ -17,10 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = (Spinner)findViewById(R.id.spinner);
+        textView = (TextView)findViewById(R.id.textView);
 
         ArrayAdapter <String>  adapterFruit = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,fruit);
-        adapterFruit.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        adapterFruit.setDropDownViewResource(android.R.layout.simple_spinner_item);   //建造好一個spinner
         spinner.setAdapter(adapterFruit);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(parent.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
 
 
 
